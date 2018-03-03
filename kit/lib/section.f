eb1b039ac24080e58df85f55cab334f12405fb30
Unknown <roger.levy@gmail.com>
2018-03-02 23:51:31 -0500

    Add section.f

---
+++ kit/lib/section.f
@@ -0,0 +1,12 @@
+create section$  256 /allot
+: (reload)  ( -- <file> <section> )
+    >in @   bl word drop  bl word count 2dup upcase section$ place  >in !  include
+    bl word drop ;
+: reload  ( -- <file> <section> )
+    ['] (reload) catch  section$ off  throw ;
+: [section]
+    section$ c@ 0= if bl word drop exit then
+    bl word count 2dup upcase section$ count compare 0= ?exit
+    begin  bl word count  2dup upcase
+        s" [SECTION]" compare 0= if recurse exit then
+    refill 0= until ;
