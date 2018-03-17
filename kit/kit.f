3b52bc677d5792e786ef9d7811f7a29c087bb6dd
Unknown <roger.levy@gmail.com>
2018-03-17 02:51:56 -0400

    Add note in version.f

--- kit/kit.f
+++ kit/kit.f
@@ -1,5 +1,5 @@
 include kit/lib/version.f
-$000600 [version] kit-version
+$000601 [version] kit-version
 
 \ Load external libraries
 [undefined] EXTERNALS_LOADED [if]  \ ensure that external libs are only ever loaded once.
--- kit/lib/version.f
+++ kit/lib/version.f
@@ -5,3 +5,12 @@
     dup $FF0000 and r@ $FF0000 and <> abort" Incompatible major version!"
     $FF00 and r> $FF00 and <> if cr ." ::: WARNING: Potential minor version incompatibility --> " including -path type then ;
 [then]
+
+\ versions are expressed in source as BCD values MMmmrr M = major, m = minor, r = revision
+\ in documentation, they're expressed as M.m.r
+\ Major versions are always source breaking
+\ Minor versions are generally additions, but also sometimes deletions, renames, and semantic changes
+\ Revisions are bugfixes, non-source-breaking changes, documentation, reorganization etc
+\ Only a discrepancy in the Major version will prevent your project from compiling.
+\ A minor version discrepency will just issue a warning.  This can help you identify culprits.
+
