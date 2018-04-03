752d0dd2663ad4c492439aa9fffbc5ec849b7a92
Roger Levy <roger.levy@gmail.com>
2018-04-03 18:55:14 -0400

    Update Linux Allegro libraries (32-bit) to 5.2.4

--- kit/plat/sflinux.f
+++ kit/plat/sflinux.f
@@ -1,2 +1,14 @@
 include kit/ans/ffl/sflinux/ffl.f   \ FFL: DOM; FFL loads FPMATH
-include kit/ext/allegro5/allegro-5.0.x.f
+include kit/ext/allegro5/allegro-5.2.x.f
+
+library libX11.so
+
+function: XOpenDisplay ( zdisplayname -- display )
+function: XDefaultScreen ( &display -- display )
+
+function: XSync ( display discard -- )
+function: XMapWindow ( display window -- )
+function: XRaiseWindow ( display  window -- )
+
+function: XSetInputFocus ( display window revert time -- )
+function: XGetInputFocus ( display &window &revert -- )
