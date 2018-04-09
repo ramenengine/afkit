ba963a3a4cde12b9940dd189cee23184812f531b
Unknown <roger.levy@gmail.com>
2018-04-08 20:10:01 -0400

    Bump to 0.6.3

--- kit/kit.f
+++ kit/kit.f
@@ -1,5 +1,5 @@
 include kit/ans/version.f
-$000602 [version] kit-ver
+$000603 [version] kit-ver
 
 \ Load external libraries
 [undefined] EXTERNALS_LOADED [if]  \ ensure that external libs are only ever loaded once.
