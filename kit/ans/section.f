7448435dda09b48622463c8765235a49dc430cf2
Roger Levy <roger.levy@gmail.com>
2018-04-20 23:59:28 -0400

    Rename RELOAD to JUST

--- kit/ans/section.f
+++ kit/ans/section.f
@@ -1,10 +1,10 @@
 [defined] [section] [if] \\ [then]
 create section$  256 /allot
-: (reload)  ( -- <file> <section> )
+: (just)  ( -- <file> <section> )
     >in @   bl word drop  bl word count 2dup upcase section$ place  >in !  include
     bl word drop ;
-: reload  ( -- <file> <section> )
-    ['] (reload) catch  section$ off  throw ;
+: just  ( -- <file> <section> )
+    ['] (just) catch  section$ off  throw ;
 : [section]
     section$ c@ 0= if bl word drop exit then
     bl word count 2dup upcase section$ count compare 0= ?exit
