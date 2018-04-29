782ca9c938e5b9fd0e33c0b9ee45db4f620b4c2f
Unknown <roger.levy@gmail.com>
2018-04-29 03:29:25 -0400

    Make SLASHES available on all plats

--- kit/ans/files.f
+++ kit/ans/files.f
@@ -44,9 +44,10 @@ decimal
       ?dup while  2swap 2drop  #1 /string
    repeat  r> 2drop ;
 
+: slashes  2dup  over + swap do  i c@ [char] / = if  [char] \ i c!  then  #1 +loop ;
+
 [defined] linux [if]
 : -filename ( a n -- a n )  2dup  [char] / ending  nip - ;
 [else]
-: slashes  2dup  over + swap do  i c@ [char] / = if  [char] \ i c!  then  #1 +loop ;
 : -filename ( a n -- a n )  slashes 2dup  [char] \ ending  nip - ;
 [then]
