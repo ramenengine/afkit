835b1871648eef219833ad9cea8a6c7f5ecf29fa
Roger Levy <roger.levy@gmail.com>
2018-05-02 22:30:23 -0400

    0.9.9

--- kit/kit.f
+++ kit/kit.f
@@ -1,5 +1,5 @@
 include kit/ans/version.f
-$000908 [version] kit-ver
+$000909 [version] kit-ver
 
 \ Load external libraries
 [undefined] EXTERNALS_LOADED [if]  \ ensure that external libs are only ever loaded once.
