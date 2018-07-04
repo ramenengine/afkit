1af06b140fafbad53159756dd582c50d545e3fda
Unknown <roger.levy@gmail.com>
2018-07-04 01:02:31 -0400

    add ?'s to kpressed and kreleased

--- lib/kb.f
+++ lib/kb.f
@@ -1,8 +1,8 @@
 : klast     kblast swap al_key_down  ;
 : kstate    kbstate swap al_key_down ;
 : kdelta    >r  r@ klast #1 and  r> kstate #1 and  - ;
-: kpressed  kdelta #1 = ;
-: kreleased kdelta #-1 = ;
+: kpressed?  kdelta #1 = ;
+: kreleased? kdelta #-1 = ;
 : alt?      <alt> kstate     <altgr> kstate   or ;
 : ctrl?     <lctrl> kstate   <rctrl> kstate   or ;
 : shift?    <lshift> kstate  <rshift> kstate  or ;
