20d84fe0592fb8d9c80640b60ec145cf36a6258d
Unknown <roger.levy@gmail.com>
2018-07-04 01:45:51 -0400

    fix kdelta (it was reversed!)

--- lib/kb.f
+++ lib/kb.f
@@ -1,6 +1,6 @@
 : klast     kblast swap al_key_down  ;
 : kstate    kbstate swap al_key_down ;
-: kdelta    >r  r@ klast #1 and  r> kstate #1 and  - ;
+: kdelta    >r  r@ kstate #1 and  r> klast #1 and  - ;
 : kpressed?  kdelta #1 = ;
 : kreleased? kdelta #-1 = ;
 : alt?      <alt> kstate     <altgr> kstate   or ;
