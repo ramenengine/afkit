67f36b5a1e6e02c670c021aefdff7123b40c315b
Unknown <roger.levy@gmail.com>
2018-07-27 01:41:20 -0400

    renamed kpressed? and kreleased? to pressed and released

--- lib/kb.f
+++ lib/kb.f
@@ -1,8 +1,8 @@
 : klast     kblast swap al_key_down  ;
 : kstate    kbstate swap al_key_down ;
 : kdelta    >r  r@ kstate #1 and  r> klast #1 and  - ;
-: kpressed?  kdelta #1 = ;
-: kreleased? kdelta #-1 = ;
+: pressed   kdelta #1 = ;
+: released  kdelta #-1 = ;
 : alt?      <alt> kstate     <altgr> kstate   or ;
 : ctrl?     <lctrl> kstate   <rctrl> kstate   or ;
 : shift?    <lshift> kstate  <rshift> kstate  or ;
