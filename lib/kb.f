15a0c376d1359683f05665142fb5fec087f5bcad
Unknown <roger.levy@gmail.com>
2018-07-29 04:11:51 -0400

    speed up / minimize stack usage of KSTATE etc

--- lib/kb.f
+++ lib/kb.f
@@ -1,5 +1,6 @@
-: klast     kblast swap al_key_down  ;
-: kstate    kbstate swap al_key_down ;
+: keydown   over #5 rshift #1 + cells + @ swap #31 and #1 swap lshift and 0<> ;
+: klast     kblast keydown ;
+: kstate    kbstate keydown ;
 : kdelta    >r  r@ kstate #1 and  r> klast #1 and  - ;
 : pressed   kdelta #1 = ;
 : released  kdelta #-1 = ;
