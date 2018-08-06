f14c486caa94a379051382431013a50bc2bf59ed
Unknown <roger.levy@gmail.com>
2018-08-06 00:11:29 -0400

    remove "#1 +"'s from stride2d.f

--- lib/stride2d.f
+++ lib/stride2d.f
@@ -1,7 +1,7 @@
 : stride2d  ( xt x y w h stridew strideh -- )  ( x y -- )
     locals| sh sw h w y x xt |
-    h #1 + y +  y do
-        w #1 + x +  x do
+    h y +  y do
+        w x +  x do
             i j  xt execute
         sw +loop
     sh +loop ;
@@ -9,8 +9,8 @@
 : break2d  ( xt pen=xy x2 y2 stridew strideh -- )  ( pen=xy w h -- )
     locals| sh sw y2 x2 xt |
     penx @
-    y2 #1 + peny @  do
-        x2 #1 + over do
+    y2 peny @  do
+        x2 over do
             i j at  sw sh  x2 y2 i j 2- 2min  xt execute
         sw +loop
     sh +loop
