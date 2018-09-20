22d9cdee078f2fa77ad035e66a9267de97b8f89f
Unknown <roger.levy@gmail.com>
2018-09-19 22:56:16 -0400

    1.1.0

--- afkit.f
+++ afkit.f
@@ -1,5 +1,5 @@
 include afkit/ans/version.f
-#1 0 0 [version]
+#1 #1 0 [version]
 
 \ Load external libraries
 [undefined] EXTERNALS_LOADED [if]  \ ensure that external libs are only ever loaded once.
