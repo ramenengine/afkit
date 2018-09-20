bc7601e53e5a78f80d31790f255bb7fb601da380
Unknown <roger.levy@gmail.com>
2018-09-19 22:54:46 -0400

    Change version incompatibility behavior
    Now Major and Minor numbers must be less than or equal to the package version

--- ans/version.f
+++ ans/version.f
@@ -4,13 +4,8 @@
     depth 6 < abort" Version number is required!"
     packver >r packver r>
     over 0 = if 2drop exit then
-    >r
-    dup $FF000000 and r@ $FF000000 and <> abort" Incompatible major version!"
-    $FFFF00 and r> $FFFF00 and <> if
-        bold
-        cr ." ::: WARNING: Potential minor version incompatibility --> " including -path type
-        normal
-    then ;
+    swap #8 rshift swap #8 rshift > abort" Incompatible version!"
+;
 [then]
 
 \ versions are expressed as three values M = major, m = minor, R = revision
@@ -18,7 +13,5 @@
 \ Major versions are always source breaking
 \ Minor versions are generally additions, but also sometimes deletions, renames, and semantic changes
 \ Revisions are bugfixes, and benign tweaks such as dox and housekeeping
-\ Only a discrepancy in the Major version will prevent your project from compiling.
-\ A minor version discrepency will just issue a warning.  This can help you identify culprits.
 
 
