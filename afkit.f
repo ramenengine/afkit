d6e8d00dffa611d3a64ac732bece18607f22bb80
Unknown <roger.levy@gmail.com>
2018-11-12 18:31:45 -0500

    add allegro state words

--- afkit.f
+++ afkit.f
@@ -211,6 +211,23 @@ interp-src blend
 : +at  ( x y -- )  penx 2+! ;
 : at@  ( -- x y )  penx 2@ ;
 
+\ State
+create states  /ALLEGRO_STATE 16 * allot
+variable >state
+
+: (state)  >state @ 15 and  /ALLEGRO_STATE *  states + ;
+
+: +state  (state) ALLEGRO_STATE_TARGET_BITMAP
+                  ALLEGRO_STATE_DISPLAY                 or
+                  ALLEGRO_STATE_BLENDER                 or
+                  ALLEGRO_STATE_NEW_FILE_INTERFACE      or
+                  ALLEGRO_STATE_TRANSFORM               or
+                  ALLEGRO_STATE_PROJECTION_TRANSFORM    or  al_store_state
+            1 >state +!
+;
+
+: -state  -1 >state +!  (state) al_restore_state ;
+
 \ --------------------------------------------------------------------------------------------------
 include afkit/piston.f
 \ --------------------------------------------------------------------------------------------------
